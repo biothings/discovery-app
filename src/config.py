7a22347e7487537641c777b963a1cb3032a96461
Marco <artofmarco@gmail.com>
2018-08-22 10:03:04 -0700

    removed cached config from github

--- src/config.py
+++
@@ -1,9 +0,0 @@
-COOKIE_SECRET = "<put some random characters here>"
-
-# your Github application Client ID
-GITHUB_CLIENT_ID = "a5d53705afb6e546fb8e"
-# your Github application Client Secret
-GITHUB_CLIENT_SECRET = "d7c4afa66f2fddd832a3a04b3fa775f59ca9db0e"
-# your Github application Callback
-GITHUB_CALLBACK_PATH = "/oauth"
-GITHUB_SCOPE = ""
