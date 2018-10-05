e2c3661a719eb2b711c09c614fbcc55f3c121f43
Marco <artofmarco@gmail.com>
2018-10-05 12:44:41 -0700

    fix added relative paths to links

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,15 +1,15 @@
 function check_user(){
-    $.ajax({url: "/user", success: function(result){
+    $.ajax({url: "./user", success: function(result){
         var html = "";
         var side_html = "";
         if (result.login){
             if (result.avatar_url){
               $('#navPhoto').attr("src", result.avatar_url);
-              html += "<li class='nav-item userButton'><a class='nav-link' id='navPhotoLink' href='/dashboard'>"+result.login+" <img id='navPhoto' class='userImage' src='"+result.avatar_url+"' alt='user photo'></a></li>";
+              html += "<li class='nav-item userButton'><a class='nav-link' id='navPhotoLink' href='./dashboard'>"+result.login+" <img id='navPhoto' class='userImage' src='"+result.avatar_url+"' alt='user photo'></a></li>";
           }
-          html += "<li class='nav-item'><a class='nav-link' href='/logout?next=" + window.location.pathname + "'>Logout</a></li>";
+          html += "<li class='nav-item'><a class='nav-link' href='./logout?next=" + window.location.pathname + "'>Logout</a></li>";
         }else{
-            html += "<li class='nav-item'><a class='nav-link' href='/oauth'>Login</a></li>";
+            html += "<li class='nav-item'><a class='nav-link' href='./oauth'>Login</a></li>";
             side_html += html;
         }
         // Append new items to navigation
