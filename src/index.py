4c300e1589730fcd6fa26718f2b3c0bad8b52fa7
Jerry <xzhou@scripps.edu>
2019-02-12 10:31:46 -0800

    Complies with Pylint recommendations

--- src/index.py
+++ src/index.py
@@ -1,14 +1,17 @@
-from biothings.web.index_base import main, options
-from biothings.web.settings import BiothingESWebSettings
-import os.path
-import config
+''' Tornado Web Server Starting Script - Application Entry Point '''
 
-web_settings = BiothingESWebSettings(config=config)
+import os.path
+
+import config
+from biothings.web.index_base import main
+from biothings.web.settings import BiothingESWebSettings
+
+WEB_SETTINGS = BiothingESWebSettings(config=config)
 
 if __name__ == '__main__':
-    (src_path, _) = os.path.split(os.path.abspath(__file__))
-    static_path = os.path.join(src_path, 'static')
-    main(web_settings.generate_app_list(),
+    (SRC_PATH, _) = os.path.split(os.path.abspath(__file__))
+    STATIC_PATH = os.path.join(SRC_PATH, 'static')
+    main(WEB_SETTINGS.generate_app_list(),
          app_settings={"cookie_secret": config.COOKIE_SECRET},
-         debug_settings={"static_path": static_path},
+         debug_settings={"static_path": STATIC_PATH},
          use_curl=True)
