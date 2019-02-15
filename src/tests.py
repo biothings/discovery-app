996da1f766decfed94ffcf8530d7bb3c3afbbf6b
Jerry <xzhou@scripps.edu>
2019-02-14 23:50:45 -0800

    Starts tester's own tornado server

--- src/tests.py
+++ src/tests.py
@@ -3,48 +3,46 @@ Automated testing for discovery-app
     > python tests.py
 or  > nosetests tests
 '''
+import json
 import os
 import unittest
-import json
 
 import requests
 from nose.tools import eq_, ok_
+from tornado.testing import AsyncHTTPTestCase
+from tornado.web import Application, create_signed_value
+from torngithub import json_encode
 
-from web.api.es import Metadata, Schema
+from biothings.utils.common import ask
+from config import COOKIE_SECRET
 from elasticsearch_dsl import Index
+from index import WEB_SETTINGS
+from web.api.es import Metadata, Schema
 
 FORCE_TEST = False
 
 
-def ask(prompt):
-    '''Prompt Yes or No, return True or False '''
-    options = 'YN'
-    while True:
-        result = input(prompt+' Continue? [%s] ' %
-                       '|'.join(list(options))).strip().upper()
-        if result in options:
-            break
-    return result == 'Y'
-
-
-class DiscoveryAppAPITest(unittest.TestCase):
-    ''' Requires a running server to test against '''
+class DiscoveryAppAPITest(AsyncHTTPTestCase):
+    ''' The tester will start its own tornado server  '''
     __test__ = True
 
-    HOST = os.getenv("DISCOVERY_HOST", "http://localhost:8000").rstrip('/')
+    HOST = os.getenv("DISCOVERY_HOST", "").rstrip('/')
     API_ENDPOINT = '/api'
     HOST += API_ENDPOINT
+
+    def get_app(self):
+        return Application(WEB_SETTINGS.generate_app_list(), cookie_secret=COOKIE_SECRET)
 
     # Setup and Teardown
 
     @classmethod
     def setUpClass(cls):
 
-        index = Index(Schema.Index.name)
+        cls.index = Index(Schema.Index.name)
 
-        if index.exists():
-            if FORCE_TEST or ask('Current indexed documents will be permenantely lost.'):
-                index.delete()
+        if cls.index.exists():
+            if FORCE_TEST or ask('Current indexed documents will be permenantely lost.') == 'Y':
+                cls.index.delete()
             else:
                 exit()
 
@@ -57,7 +55,7 @@ class DiscoveryAppAPITest(unittest.TestCase):
         # add a document
         url = 'https://raw.githubusercontent.com/namespacestd0/mygene.info/master/README.md'
         meta = Metadata(username='namespacestd', slug='dev', url=url)
-        schema = Schema(clses=['biothings', 'smartapi'],
+        schema = Schema(selfes=['biothings', 'smartapi'],
                         props='es-dsl', _meta=meta)
         schema.save()
         cls.testset.append(schema)
@@ -66,7 +64,7 @@ class DiscoveryAppAPITest(unittest.TestCase):
         url = ('https://raw.githubusercontent.com/data2health/'
                'schemas/biothings/biothings/biothings_curie.jsonld')
         meta = Metadata(username='data2health', slug='d2h', url=url)
-        schema = Schema(clses='biothings', _meta=meta)
+        schema = Schema(selfes='biothings', _meta=meta)
         schema.save()
         cls.testset.append(schema)
 
@@ -76,37 +74,41 @@ class DiscoveryAppAPITest(unittest.TestCase):
 
     # Helper functions
 
-    @staticmethod
-    def get_status_code(url, status_code):
-        ''' make a GET request and return json if the status codes match '''
-        res = requests.get(url)
-        assert res.status_code == status_code, "status {} != {} for GET to url: {}".format(
-            res.status_code, status_code, url)
-        return res.json()
+    def get_status_code(self, url, status_code):
+        ''' make a GET request and return the response body if the status codes match '''
+        res = self.fetch(url)
+        assert res.code == status_code, "status {} != {} for GET to url: {}".format(
+            res.code, status_code, url)
+        return res.body
 
-    @staticmethod
-    def post_status_code(url, data, status_code):
-        ''' make a POST request and return json if the status codes match '''
-        headers = {'Content-type': 'application/x-www-form-urlencoded'}
-        res = requests.post(url, data=json.dumps(data), headers=headers)
-        assert res.status_code == status_code, "status {} != {} for url: {}\nparams: {}".format(
-            res.status_code, status_code, url, data)
-        return res.json()
+    def post_status_code(self, url, body, status_code):
+        ''' make an authenticated POST request and return json if the status codes match '''
 
-    @classmethod
-    def get_ok(cls, url):
+        cookie_name, cookie_value = 'user', 'tester'
+        secure_cookie = create_signed_value(
+            COOKIE_SECRET,
+            cookie_name,
+            json_encode(cookie_value))
+        headers = {'Content-type': 'application/x-www-form-urlencoded',
+                   'Cookie': '='.join((cookie_name, secure_cookie.decode()))}
+        res = self.fetch(url, method='POST',
+                         body=json.dumps(body), headers=headers)
+        assert res.code == status_code, "status {} != {} for url: {}\nparams: {}".format(
+            res.code, status_code, url, body)
+        return res.body
+
+    def get_json_ok(self, url):
         ''' make a GET request and return json if server responds okay '''
-        return cls.get_status_code(url, 200)
+        return json.loads(self.get_status_code(url, 200))
 
-    @classmethod
-    def post_ok(cls, url, data):
+    def post_json_ok(self, url, data):
         ''' make a POST request and return json if server responds okay '''
-        return cls.post_status_code(url, data, 200)
+        return json.loads(self.post_status_code(url, data, 200))
 
-    @classmethod
-    def query_has_hits(cls, query_keyword, endpoint='query'):
+    def query_has_hits(self, query_keyword, endpoint='query'):
         ''' make a GET request to a query endpoint and assert positive hits '''
-        dic = cls.get_ok(cls.HOST + '/' + endpoint + '?q=' + query_keyword)
+        dic = self.get_json_ok(
+            self.HOST + '/' + endpoint + '?q=' + query_keyword)
         ok_(dic.get('total', 0) > 0)
         ok_(dic.get('hits', []))
         return dic
@@ -144,14 +146,15 @@ class DiscoveryAppAPITest(unittest.TestCase):
     def test_handlers_registry_get(self):
         ''' asserts get request to registry retrives documents
         acknowledges timestamp will not be in datetime format in res'''
-        res = self.get_ok(self.HOST+'/registry/'+self.testset[0].meta.id)
+        res = self.get_json_ok(self.HOST+'/registry/'+self.testset[0].meta.id)
         eq_(res['_meta']['url'], self.testset[0].to_dict()['_meta']['url'])
 
     def test_handlers_registry_post(self):
-        ''' asserts props and clses (p&c) are optional,
+        ''' asserts props and selfes (p&c) are optional,
         asserts p&c take both str and list,
         asserts p&c are converted to lower cases,
         asserts update to existing doc works '''
+        # it's normal that ~raw field will be empty due to implementation
         doc_1 = {'url': 'http://example.com/',
                  'slug': 'com',
                  'props': 'ebay'}
@@ -159,11 +162,11 @@ class DiscoveryAppAPITest(unittest.TestCase):
                  'slug': 'org',
                  'props': ['cvs'],
                  'clses': ['costco', 'MTS']}
-        ok_(self.post_ok(self.HOST+'/registry', data=doc_1)['success'])
-        ok_(self.post_ok(self.HOST+'/registry', data=doc_2)['success'])
-        self.query_has_hits(query_keyword='mts') # not MTS
+        ok_(self.post_json_ok(self.HOST+'/registry', data=doc_1)['success'])
+        ok_(self.post_json_ok(self.HOST+'/registry', data=doc_2)['success'])
+        self.query_has_hits(query_keyword='mts')  # not MTS
         doc_2['slug'] = 'us'
-        self.post_ok(self.HOST+'/registry', data=doc_2)
+        self.post_json_ok(self.HOST+'/registry', data=doc_2)
         self.query_has_hits(query_keyword='_meta.slug:us')
 
 
