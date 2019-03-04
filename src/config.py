bbc943e9e2d08cfc56ec6836a9b755894a3acbbc
Jerry <xzhou@scripps.edu>
2019-03-04 09:24:05 -0800

    Add helpful messages in the config file

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 # pylint: disable=unused-wildcard-import, wildcard-import, unused-import
-''' Discovery App Configuration
-Compatible with Biothings Web Component '''
+
+''' Discovery App Configuration '''
+
 from biothings.web.api.es.handlers import QueryHandler
 from biothings.web.settings.default import *
 from config_key import COOKIE_SECRET, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET
@@ -8,19 +9,29 @@ from web.api.handlers import RegistryHandler
 from web.handlers import APP_LIST as WEB_ENDPOINTS
 
 # *****************************************************************************
-# Elasticsearch variables
+# Credentials
+# *****************************************************************************
+# Define in config_key.py:
+#   COOKIE_SECRET
+#   GITHUB_CLIENT_ID
+#   GITHUB_CLIENT_SECRET
+
+# *****************************************************************************
+# Elasticsearch
 # *****************************************************************************
 ES_INDEX = 'discovery'
 ES_DOC_TYPE = 'schema'
 
 # *****************************************************************************
-# App URL Patterns
+# Tornado URL Patterns
 # *****************************************************************************
 API_ENDPOINTS = [
     (r"/api/query/?", QueryHandler),
     (r"/api/registry/?", RegistryHandler),
-    (r"/api/registry/(.+)/?", RegistryHandler)
-]
+    (r"/api/registry/(.+)/?", RegistryHandler)]
 APP_LIST = API_ENDPOINTS + WEB_ENDPOINTS
 
+# *****************************************************************************
+# Biothings Query Settings
+# *****************************************************************************
 DISABLE_CACHING = True
