45a7e7566b0d50dd9ea482c00e31b74b83d7ba43
Jerry <xzhou@scripps.edu>
2019-03-15 00:36:19 -0700

    Simplify imports

--- discovery/index.py
+++ discovery/index.py
@@ -5,14 +5,12 @@ import os
 from biothings.web.index_base import main
 from biothings.web.settings import BiothingESWebSettings
 
-from . import config
-
-WEB_SETTINGS = BiothingESWebSettings(config=config)
+WEB_SETTINGS = BiothingESWebSettings(config='config')
 
 if __name__ == '__main__':
     (SRC_PATH, _) = os.path.split(os.path.abspath(__file__))
     STATIC_PATH = os.path.join(SRC_PATH, 'static')
     main(WEB_SETTINGS.generate_app_list(),
-         app_settings={"cookie_secret": config.COOKIE_SECRET},
+         app_settings={"cookie_secret": WEB_SETTINGS.COOKIE_SECRET},
          debug_settings={"static_path": STATIC_PATH},
          use_curl=True)
