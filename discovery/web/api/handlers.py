166a43d084744578332d1ee3e78d1d71d1d8a823
Jerry <xzhou@scripps.edu>
2019-03-18 14:15:41 -0700

    Support Regsitry PUT request

--- discovery/web/api/handlers.py
+++ discovery/web/api/handlers.py
@@ -53,26 +53,51 @@ class RegistryHandler(BaseHandler):
         self.return_json(sch.to_dict())
 
     def put(self, api_id):
-        ''' Update existing only '''
+        ''' Update existing document's slug only
+            Use POST to create a document
+            to gurantee _id corresponds to its meta url '''
 
-        sch = Schema.get(id=api_id)
-        # check existance
-
-        # extract what to update
-
-        # update and save
-
-        # return response
-
-        # Validate
-        req = tornado.escape.json_decode(self.request.body)
-        url = req.get('url', '').lower()  # TODO cannot edit url
-        slug = req.get('slug', '').lower()
-        if not url or not slug:
+        # Authenticate
+        if not self.current_user:
             res = {'success': False,
-                   'error': 'Parameters "url" and/or "slug" not found.'}
+                   'error': 'Login with your Github account first.'}
+            self.return_json(res, status_code=401)
+            return
+
+        # Validate document existance by id
+        sch = Schema.get(id=api_id)
+        if not sch:
+            res = {'success': False,
+                   'error': 'Document does not exisit.'}
+            self.return_json(res, status_code=404)
+
+        # Validate edit permission
+        if sch['_meta'].username != self.current_user['login']:
+            res = {'success': False,
+                   'error': 'Document does not belong to the logged-in user.'}
+            self.return_json(res, status_code=403)
+
+        # Parse request body
+        req = tornado.escape.json_decode(self.request.body)
+        url = req.get('url', '').lower()
+        if url:
+            res = {'success': False,
+                   'error': 'URL change requires recreating the document.'}
+            self.return_json(res, status_code=403)
+            return
+        slug = req.get('slug', '').lower()
+        if not slug:
+            res = {'success': False,
+                   'error': 'Parameters "slug" not found.'}
             self.return_json(res, status_code=400)
             return
+
+        # update and save
+        sch['_meta'].slug = slug
+        result = sch.save()
+        assert not result
+        res = {'success': True}
+        self.return_json(res, status_code=200)
 
     def delete(self, api_id):
         ''' Delete a document by es _id '''
@@ -88,9 +113,8 @@ class RegistryHandler(BaseHandler):
             res = {'success': False,
                    'error': 'Document does not exisit.'}
             self.return_json(res, status_code=404)
-            # TODO add warning on tornado
 
-        if sch['_meta'].username != self.current_user:
+        if sch['_meta'].username != self.current_user['login']:
             res = {'success': False,
                    'error': 'Document does not belong to the logged-in user.'}
             self.return_json(res, status_code=403)
@@ -100,19 +124,21 @@ class RegistryHandler(BaseHandler):
 
 
 class ProxyHandler(BaseHandler):
+    ''' retrive a document from a remote server to bypass same origin policy '''
     async def get(self):
-        # input validation
-        url = self.get_argument("url", "/")
-        # fetch attempt
 
-        async def f():
-            http_client = tornado.httpclient.AsyncHTTPClient()
-            try:
-                response = await http_client.fetch(url)
-            except Exception as e:
-                print("Error: %s" % e)
-            else:
-                return response.body
-        # response validation
-        # respond TODO
-        self.write(f())
+        # TODO user validation
+
+        # TODO input validation
+        url = self.get_argument("url", "/")
+
+        # TODO header forward
+
+        # fetch attempt
+        http_client = tornado.httpclient.AsyncHTTPClient()
+        response = await http_client.fetch(url)
+
+        # TODO response validation
+
+        self.write(response.body)
+        self.finish()
