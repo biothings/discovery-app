41a4e5185b2f6c8e809ccbadf9ecce25d759f0e2
Jerry <xzhou@scripps.edu>
2019-03-18 17:28:06 -0700

    Improve project structure

--- discovery/index.py
+++ discovery/index.py
@@ -1,6 +1,7 @@
 ''' Tornado Web Server Starting Script - Application Entry Point '''
 
 import os
+import sys
 
 from biothings.web.index_base import main
 from biothings.web.settings import BiothingESWebSettings
@@ -8,7 +9,10 @@ from biothings.web.settings import BiothingESWebSettings
 WEB_SETTINGS = BiothingESWebSettings(config='config')
 
 if __name__ == '__main__':
-    (SRC_PATH, _) = os.path.split(os.path.abspath(__file__))
+    SRC_PATH = os.path.dirname(os.path.abspath(__file__))
+    PROJ_PATH = os.path.dirname(SRC_PATH)
+    if PROJ_PATH not in sys.path:
+        sys.path.append(PROJ_PATH)
     STATIC_PATH = os.path.join(SRC_PATH, 'static')
     main(WEB_SETTINGS.generate_app_list(),
          app_settings={"cookie_secret": WEB_SETTINGS.COOKIE_SECRET},
