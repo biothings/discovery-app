c96fe32c51a2e9d384a08e0d4832fd5cac4b10ba
Jerry <xzhou@scripps.edu>
2019-06-12 18:41:58 -0700

    Rename quick start script

---
+++ discovery/scripts/quick_start.py
@@ -0,0 +1,33 @@
+'''
+    Empty Index Creator
+'''
+
+from discovery.web.api.es.doc import Class, Schema
+from elasticsearch_dsl import Index
+
+
+def main():
+    '''
+        Quick Setup Script
+
+        - Create two empty indexes
+        - Meet minimum running requirement
+
+    '''
+
+    index_primary = Index('discover_schema')
+    index_secondary = Index('discover_class')
+
+    # remove existing index
+    if index_primary.exists():
+        index_primary.delete()
+
+    if index_secondary.exists():
+        index_secondary.delete()
+
+    Schema.init()
+    Class.init()
+
+
+if __name__ == "__main__":
+    main()
