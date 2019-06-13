8868a86d19ad6b4a87353f8352c40f9d6b2e61f8
Jerry <xzhou@scripps.edu>
2019-06-13 15:17:51 -0700

    Compatible with future biothings_schema release

--- discovery/scripts/schema_org.py
+++ discovery/scripts/schema_org.py
@@ -8,7 +8,6 @@ from discovery.web.api.es.doc import Schema
 from discovery.web.api.handlers import populate_class_index
 
 SCHEMA_ORG_URL = "http://schema.org/version/latest/schema.jsonld"
-# SCHEMA_ORG_URL = "https://raw.githubusercontent.com/data2health/schemas/biothings/biothings/biothings_curie_kevin.jsonld"
 
 
 def main():
