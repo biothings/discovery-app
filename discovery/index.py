10d9205067eda23c0bbc4bfa6fb41c98a18ac2ce
Jerry <xzhou@scripps.edu>
2019-06-12 18:41:22 -0700

    Remove dynamic environment setup

--- discovery/index.py
+++ discovery/index.py
@@ -1,7 +1,6 @@
 ''' Tornado Web Server Starting Script - Application Entry Point '''
 
 import os
-import sys
 
 from biothings.web.index_base import main
 from biothings.web.settings import BiothingESWebSettings
@@ -10,9 +9,6 @@ WEB_SETTINGS = BiothingESWebSettings(config='config')
 
 if __name__ == '__main__':
     SRC_PATH = os.path.dirname(os.path.abspath(__file__))
-    PROJ_PATH = os.path.dirname(SRC_PATH)
-    if PROJ_PATH not in sys.path:
-        sys.path.append(PROJ_PATH)
     STATIC_PATH = os.path.join(SRC_PATH, 'static')
     main(WEB_SETTINGS.generate_app_list(),
          app_settings={"cookie_secret": WEB_SETTINGS.COOKIE_SECRET},
