046d5678b48bd27b331875925c9dab192a8b03b2
Jerry <xzhou@scripps.edu>
2019-06-14 19:13:20 -0700

    Refine logging output

--- discovery/scripts/schema_org.py
+++ discovery/scripts/schema_org.py
@@ -14,18 +14,23 @@ def main():
     '''
         Setup Script
     '''
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s - %(module)s - %(levelname)s - %(message)s')
-    logging.info("Start indexing schema.org definitions.")
+    logger = logging.getLogger('discovery.scripts.schema_org')
+    logger.setLevel(logging.DEBUG)
+
+    logger.info("Start indexing schema.org definitions.")
 
     schema = Schema('schema', SCHEMA_ORG_URL, 'schema_org_auto_indexer')
-    schema.save()
+    ans = schema.save()
+    logger.debug("Indexed schema 'schema' (new document created:%s).", ans)
 
     populate_class_index(schema)
 
-    logging.info("Finished indexing schema.org definitions.")
+    logger.info("Finished indexing schema.org definitions.")
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.WARNING,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logging.getLogger("discovery").setLevel(logging.INFO)
     main()
