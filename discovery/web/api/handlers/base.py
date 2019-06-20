d29dda39f9c7e83cba33571e5a0c98e8a673663f
Jerry <xzhou@scripps.edu>
2019-06-20 11:58:22 -0700

    Refactor api handlers

--- discovery/web/api/handlers/base.py
+++ discovery/web/api/handlers/base.py
@@ -1,11 +1,28 @@
 
-from tornado.escape import json_decode
+import copy
+import json
 import logging
+
+import requests
+from tornado.escape import json_decode
+
 from biothings.web.api.helper import BaseHandler
+from biothings_schema import Schema as SchemaParser
+from discovery.web.api.es.doc import Class
 
 
 # pylint: disable=abstract-method, arguments-differ
 class APIBaseHandler(BaseHandler):
+
+    _PARSER = SchemaParser()
+
+    @classmethod
+    def get_parser(cls, doc):
+
+        parser = copy.deepcopy(cls._PARSER)
+        parser.load_schema(doc)
+
+        return parser
 
     def get_current_user(self):
 
@@ -41,3 +58,36 @@ class APIBaseHandler(BaseHandler):
 
         template.update(kwargs)
         self.finish(template)
+
+    def import_from_url(self, url):
+
+        try:
+
+            response = requests.get(url, timeout=5)
+            parser = self.get_parser(response.json())
+
+            logger = logging.getLogger(__name__)
+            logger.info('Loaded %s.', url)
+
+            classes = Class.import_from_parser(parser)
+            logger.info('Parsed %s classes.', len(classes))
+
+        except requests.RequestException as exc:
+
+            self.send_error(reason=str(exc), status_code=400)
+            return
+
+        except json.decoder.JSONDecodeError:
+
+            self.send_error(reason='not a valid json', status_code=400)
+            return
+
+        except BaseException:
+
+            logging.exception("parser_exception")
+            self.send_error(reason='parser_exception')
+            return
+
+        else:
+
+            return classes
