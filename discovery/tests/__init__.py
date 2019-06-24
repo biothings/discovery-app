f8d122ed4e5f076872e123e90f310a6ff6ade455
Jerry <xzhou@scripps.edu>
2019-06-24 11:57:27 -0700

    Remove package level setup

--- discovery/tests/__init__.py
+++ discovery/tests/__init__.py
@@ -10,35 +10,12 @@ from discovery.web.api.es.doc import Class, Schema
 from elasticsearch_dsl import Index
 
 
-def setup():
-    ''' package level setup '''
-
-    print("Existing discovery-app es indexes will be deleted.")
-    print("Terminate the program to avoid losing data.")
-    input("Press Enter to continue...")
-
-    index_schema = Index('discover_schema')
-    index_class = Index('discover_class')
-
-    # remove existing index
-    if index_schema.exists():
-        index_schema.delete()
-
-    if index_class.exists():
-        index_class.delete()
-
-    # create new indexes
-    Schema.init()
-    Class.init()
-
-
 def run(testname):
     ''' run tests with fixtures '''
 
     if inspect.isclass(testname):
         testname = testname.__name__
 
-    setup()
     print()
     print(testname)
     print()
