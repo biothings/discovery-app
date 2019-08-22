55152e3a167e6f6e051f6263d0243b95516297d6
Jerry <xzhou@scripps.edu>
2019-08-21 19:48:22 -0700

    Provide graceful es reconnection behavior

---
+++ discovery/api/handlers/query.py
@@ -0,0 +1,8 @@
+from biothings.web.api.es.handlers import QueryHandler as BiothingsQueryHandler
+
+from .base import APIBaseHandler
+
+
+class QueryHandler(BiothingsQueryHandler, APIBaseHandler):
+
+    pass
--- discovery/scripts/setup.py
+++ discovery/scripts/setup.py
@@ -5,6 +5,7 @@
 import logging
 
 from biothings_schema import Schema as SchemaParser
+from elasticsearch_dsl import Index
 
 from discovery.api.es.doc import DatasetMetadata, Schema, SchemaClass
 
@@ -26,6 +27,29 @@ def index_schema_org(lazy=False):
     logger.info("Indexed 'schema'.")
 
 
+def es_data_setup():
+    '''
+        Called when web server starts.
+    '''
+
+    try:
+
+        if not Index('discover_schema').exists():
+            Schema.init()
+
+        if not Index('discover_class').exists():
+            SchemaClass.init()
+
+        if not Index('discover_metadata').exists():
+            DatasetMetadata.init()
+
+        index_schema_org(True)
+
+    except Exception as exc:
+
+        logging.warning(exc)
+
+
 if __name__ == "__main__":
     logging.basicConfig(
         level=logging.INFO,
@@ -35,4 +59,4 @@ if __name__ == "__main__":
     Schema.init()
     SchemaClass.init()
     DatasetMetadata.init()
-    index_schema_org(True)
+    index_schema_org()
