72fd4f550c9d6cf52e656328105d31e86d5f9a56
Jerry <xzhou@scripps.edu>
2019-08-21 18:38:10 -0700

    Use consistent es connection settings

--- discovery/api/es/__init__.py
+++
@@ -1,6 +0,0 @@
-''' Setup ES '''
-
-from elasticsearch_dsl.connections import connections
-
-# Default elasticsearch connection
-connections.create_connection(hosts=['localhost'], timeout=20)
--- discovery/index.py
+++ discovery/index.py
@@ -5,6 +5,7 @@ import os
 
 from biothings.web.index_base import main
 from biothings.web.settings import BiothingESWebSettings
+from elasticsearch_dsl.connections import connections
 
 WEB_SETTINGS = BiothingESWebSettings(config='config')
 SRC_PATH = os.path.dirname(os.path.abspath(__file__))
@@ -12,6 +13,10 @@ STATIC_PATH = os.path.join(SRC_PATH, 'web', 'static')
 
 if __name__ == '__main__':
     logging.captureWarnings(True)
+    connections.create_connection(
+        hosts=[WEB_SETTINGS.ES_HOST],
+        timeout=WEB_SETTINGS.ES_CLIENT_TIMEOUT,
+        max_retries=1)
     main(WEB_SETTINGS.generate_app_list(),
          app_settings={"cookie_secret": WEB_SETTINGS.COOKIE_SECRET},
          debug_settings={"static_path": STATIC_PATH},
