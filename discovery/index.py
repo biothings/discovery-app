5798aa5c9488cddee2f74cb2f9b44e88b4775f55
Jerry <xzhou@scripps.edu>
2019-08-21 19:08:39 -0700

    Allow specifying multiple possible es hosts

--- discovery/index.py
+++ discovery/index.py
@@ -4,18 +4,20 @@ import logging
 import os
 
 from biothings.web.index_base import main
-from biothings.web.settings import BiothingESWebSettings
 from elasticsearch_dsl.connections import connections
 
-WEB_SETTINGS = BiothingESWebSettings(config='config')
+from discovery.web.settings import DiscoveryWebSettings
+
+WEB_SETTINGS = DiscoveryWebSettings(config='config')
 SRC_PATH = os.path.dirname(os.path.abspath(__file__))
 STATIC_PATH = os.path.join(SRC_PATH, 'web', 'static')
 
 if __name__ == '__main__':
     logging.captureWarnings(True)
     connections.create_connection(
-        hosts=[WEB_SETTINGS.ES_HOST],
+        hosts=WEB_SETTINGS.ES_HOST,
         timeout=WEB_SETTINGS.ES_CLIENT_TIMEOUT,
+        sniff_on_start=True,
         max_retries=1)
     main(WEB_SETTINGS.generate_app_list(),
          app_settings={"cookie_secret": WEB_SETTINGS.COOKIE_SECRET},
