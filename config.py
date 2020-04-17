9b5aaf4c1ba36490340661ff707f6e94ef4e9c1c
Jerry <xzhou@scripps.edu>
2020-04-16 23:29:29 -0700

    Restructure application and support biothings 0.7

---
+++ config.py
@@ -0,0 +1,42 @@
+
+''' Discovery App Configuration '''
+
+
+from config_key import COOKIE_SECRET, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET
+
+# *****************************************************************************
+# Credentials
+# *****************************************************************************
+# Define in <project_folder>/config_key.py:
+#   COOKIE_SECRET = "REDACTED"
+#   GITHUB_CLIENT_ID = "REDACTED"
+#   GITHUB_CLIENT_SECRET = "REDACTED"
+
+# *****************************************************************************
+# Elasticsearch
+# *****************************************************************************
+ES_INDEX = 'discover_schema_class'
+ES_DOC_TYPE = 'schema'
+ES_CLIENT_TIMEOUT = 10
+
+# *****************************************************************************
+# Tornado URL Patterns
+# *****************************************************************************
+API_PREFIX = 'api'
+API_VERSION = ''
+APP_LIST = [
+    (r"/api/query/?", "biothings.web.api.es.handlers.QueryHandler"),
+    (r"/api/registry/([^/]+)/([^/]+)/?", "discovery.web.api.RegistryHandler"),
+    (r"/api/registry/([^/]+)/?", "discovery.web.api.RegistryHandler"),
+    (r"/api/registry/?", "discovery.web.api.RegistryHandler"),
+    (r"/api/dataset/([^/]+)/?", "discovery.web.api.MetadataHandler"),
+    (r"/api/dataset/?", "discovery.web.api.MetadataHandler"),
+    (r"/api/view/?", "discovery.web.api.SchemaViewHandler")
+]
+
+# *****************************************************************************
+# Biothings SDK Settings
+# *****************************************************************************
+ACCESS_CONTROL_ALLOW_METHODS = 'HEAD,GET,POST,DELETE,PUT,OPTIONS'
+ES_QUERY_BUILDER = "discovery.query.DiscoveryQueryBuilder"
+DISABLE_CACHING = True
