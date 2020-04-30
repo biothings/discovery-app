265aa59cd889c9d558efc4df3f5f254f22a2b7dd
Jerry <xzhou@scripps.edu>
2020-04-29 18:56:56 -0700

    Add SAML logout cookie handling

--- discovery/web/handlers.py
+++ discovery/web/handlers.py
@@ -156,7 +156,8 @@ class LoginHandler(BaseHandler):
 
 class LogoutHandler(BaseHandler):
     def get(self):
-        self.clear_cookie("user")
+        self.clear_cookie("user") # oauth
+        self.clear_cookie("session") # saml
         self.redirect(self.get_argument("next", "/"))
 
 
