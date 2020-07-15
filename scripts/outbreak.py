0305c7c2b89c3fa95e7cad0824c108643b1531ee
Jerry <xzhou@scripps.edu>
2020-07-15 14:29:01 -0700

    Add outbreak reindex script

---
+++ scripts/outbreak.py
@@ -0,0 +1,106 @@
+"""
+    Add dataset metadata to outbreak.api
+
+    Assume elasticsearch related setups exist on the target server.
+
+    python -m scripts.outbreak --help
+
+"""
+
+import logging
+from datetime import datetime
+import requests
+from elasticsearch import Elasticsearch, RequestError
+from tornado.options import options, parse_command_line
+
+from discovery.data.dataset import DatasetMetadata
+
+options.define('target_host', default='api.outbreak.info')
+
+MAPPING_URL = 'https://raw.githubusercontent.com/SuLab/outbreak.info-resources/master/outbreak_resources_es_mapping.json'
+INDEX_PREFIX = 'outbreak-dataset-'
+INDEX_ALIAS = 'outbreak-resources-dataset'
+
+
+def main():
+
+    parse_command_line()
+    client = Elasticsearch(options.target_host)
+
+    # create index
+    datestring = ''.join(str(item) for item in datetime.now().timetuple()[:-1])
+    index_name = INDEX_PREFIX + datestring
+    _ = client.indices.create(index_name, {
+        "settings": {
+            "query": {
+                "default_field": "all"
+            },
+            "default_pipeline": "resources-common",
+            "analysis": {
+                "normalizer": {
+                    "keyword_lowercase_normalizer": {
+                        "filter": [
+                            "lowercase"
+                        ],
+                        "type": "custom",
+                        "char_filter": []
+                    }
+                },
+                "analyzer": {
+                    "string_lowercase": {
+                        "filter": "lowercase",
+                        "tokenizer": "keyword"
+                    },
+                    "whitespace_lowercase": {
+                        "filter": "lowercase",
+                        "tokenizer": "whitespace"
+                    }
+                }
+            }
+        },
+        "mappings": {
+            "_doc": {
+                "properties": requests.get(MAPPING_URL).json(),
+                "dynamic": False
+            }
+        }
+    })
+    logging.debug(_)
+
+    for doc in DatasetMetadata.search().scan():
+        dic = doc.to_json()
+        if dic.get('@type') == 'outbreak:Dataset':
+            try:
+                client.index(index_name, dic, id=doc.meta.id)
+            except RequestError as err:
+                logging.error(err.info)
+
+    # switch index alias
+    _ = client.indices.update_aliases({
+        "actions": [
+            {
+                "remove": {
+                    "index": INDEX_PREFIX + '*',
+                    "alias": INDEX_ALIAS
+                }
+            },
+            {
+                "add": {
+                    "index": index_name,
+                    "alias": INDEX_ALIAS,
+                }
+            }
+        ]
+    })
+    logging.debug(_)
+
+    # delete old index
+    indices = list(client.indices.get(INDEX_PREFIX + '*').keys())
+    indices.remove(index_name)
+    indices = ','.join(indices)
+    _ = client.indices.delete(indices)
+    logging.info(_)
+
+
+if __name__ == "__main__":
+    main()
