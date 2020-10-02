420068072354c5513375ee16a4e7ac4986c1d025
Marco <artofmarco@gmail.com>
2020-10-02 10:05:48 -0700

    add updated GH handlers and user interface for GH interaction via editor

--- discovery/web/api/github.py
+++ discovery/web/api/github.py
@@ -13,9 +13,10 @@ logger = logging.getLogger(__name__)
 class GHHandler(APIBaseHandler):
 
     @github_authenticated
-    def get(self):
+    def get(self, repo_name=None):
         """
-        Delete repo
+        Check repo existence
+        or get all public repos
         {
             "name": repo name
         }
@@ -24,27 +25,65 @@ class GHHandler(APIBaseHandler):
         g = Github(user['access_token'])
         # authenticated user
         auth_user = g.get_user()
-        repo_name = self.args.name
+
         if not repo_name:
-            raise HTTPError(400, reason=f"Repo name not provided")
-        try:
-            repo = auth_user.get_repo(full_name=repo_name)
-        except GithubException as e:
-            raise HTTPError(400, reason=e)
-        except Exception as exc:  # unexpected
-            raise HTTPError(500, reason=str(exc))
-        if repo.full_name:
-            self.finish({
-                'success': True,
-                'exists': True,
-                'msg': f"{repo_name} exists"
-            })
+            print("NO REPO NAME")
+            list = []
+            if auth_user.login:
+                try:
+                    repos = auth_user.get_repos()
+                except GithubException:
+                    self.finish({
+                        'success': True,
+                        'exists': False,
+                        'msg': list
+                    })
+                except Exception as exc:  # unexpected
+                    raise HTTPError(500, reason=str(exc))
+                else:
+
+                    for repo in repos:
+                        list.append(repo.name)
+                    self.finish({
+                        'success': True,
+                        'exists': True,
+                        'msg': list
+                    })
+            else:
+                raise HTTPError(400, reason=f"Unable to authenticate user")
         else:
-            self.finish({
-                'success': True,
-                'exists': False,
-                'msg': f"{repo_name} does not exist"
-            })
+            print('REPO NAME', repo_name)
+            if auth_user.login:
+                print(auth_user)
+                try:
+                    repo = auth_user.get_repo(repo_name)
+                except GithubException.UnknownObjectException:
+                    self.finish({
+                        'success': True,
+                        'exists': False,
+                        'msg': f"{repo_name} does not exist"
+                    })
+                except Exception as exc:  # unexpected
+                    raise HTTPError(500, reason=str(exc))
+                else:
+                    if repo.full_name:
+                        contents = repo.get_contents("")
+                        files = []
+                        for content_file in contents:
+                            files.append(content_file.path)
+                        self.finish({
+                            'success': True,
+                            'exists': True,
+                            'msg': files
+                        })
+                    else:
+                        self.finish({
+                            'success': True,
+                            'exists': False,
+                            'msg': f"{repo_name} does not exist"
+                        })
+            else:
+                raise HTTPError(400, reason=f"Unable to authenticate user")
 
 
     @github_authenticated
@@ -79,7 +118,8 @@ class GHHandler(APIBaseHandler):
     @github_authenticated
     def post(self):
         """
-        Add repo to GH
+        Create new repo with file
+        or add file to existing repo
         {
             "name": repo name
             "file": name of jsonld schema
@@ -90,6 +130,8 @@ class GHHandler(APIBaseHandler):
             self.args = json.loads(self.request.body)
         repo_name = self.args.get('name', None)
         file_name = self.args.get('file', None)
+        msg = self.args.get('comment', "added by Data Discovery Engine")
+        existing = self.args.get('existing', False)
         # data must be encoded
         data = self.args.get('data', None)
         if data:
@@ -107,22 +149,34 @@ class GHHandler(APIBaseHandler):
         # authenticated user
         auth_user = g.get_user()
         if auth_user.login:
-            try:
-                print('auth_user',auth_user)
-                repo = auth_user.create_repo(repo_name)
-                # print("REPO CREATED ",repo.full_name)
-            except GithubException as e:
-                raise HTTPError(400, reason=e)
-            except Exception as exc:  # unexpected
-                raise HTTPError(500, reason=str(exc))
+
+            if existing:
+                try:
+                    repo = auth_user.get_repo(repo_name)
+                except GithubException.UnknownObjectException:
+                    self.finish({
+                        'success': False,
+                        'exists': True,
+                        'msg': f"Repo {repo_name} already exists"
+                    })
+                except Exception as exc:  # unexpected
+                    raise HTTPError(500, reason=str(exc))
+            else:
+                try:
+                    print('auth_user',auth_user)
+                    repo = auth_user.create_repo(repo_name)
+                    # print("REPO CREATED ",repo.full_name)
+                except GithubException as e:
+                    raise HTTPError(400, reason=e)
+                except Exception as exc:  # unexpected
+                    raise HTTPError(500, reason=str(exc))
 
         else:
             raise HTTPError(400, reason=f"Unable to authenticate user")
 
         if repo:
-            msg = "added by Data Discovery Engine"
             path = file_name
-            file = repo.create_file(path, msg, content_encoded, branch='master')
+            file = repo.create_file(path, msg, content_encoded)
             # returns
             # { ‘content’: ContentFile:, ‘commit’: Commit}
             if file:
@@ -144,12 +198,19 @@ class GHHandler(APIBaseHandler):
             "name": repo name
             "file": name or path to file
             "data": file content- needs to be encoded
+            'existing': existing repo,
+            'comment': commit comment,
+            'existing_file': update existing file?
         }
         """
         if self.request.headers['Content-Type'] == 'application/json':
             self.args = json.loads(self.request.body)
         repo_name = self.args.get('name', None)
         file_name = self.args.get('file', None)
+        msg = self.args.get('comment', "updated via Data Discovery Engine")
+        existing = self.args.get('existing', True)
+        existing_file = self.args.get('existing_file', True)
+
         # data must be encoded
         data = self.args.get('data', None)
         if data:
@@ -159,45 +220,43 @@ class GHHandler(APIBaseHandler):
         if not repo_name:
             raise HTTPError(400, reason=f"Repo name not provided")
 
-        logging.info(msg="Repo name "+repo_name)
+        logging.info(msg="Updating file on repo.name: "+repo_name)
         user = json.loads(self.get_secure_cookie("user"))
         g = Github(user['access_token'])
         # authenticated user
         auth_user = g.get_user()
         if auth_user.login:
             try:
-                repo = auth_user.create_repo(repo_name)
+                repo = auth_user.get_repo(repo_name)
             except GithubException as e:
                 raise HTTPError(400, reason=e)
             except Exception as exc:  # unexpected
                 raise HTTPError(500, reason=str(exc))
         else:
             raise HTTPError(400, reason=f"Unable to authenticate user")
-        if repo:
+        if repo and existing_file:
+            file_path = file_name
             try:
-                file_exists = repo.get_file_contents(file_name)
+                file = repo.get_contents(file_path)
             except GithubException as e:
                 raise HTTPError(400, reason=e)
             except Exception as exc:  # unexpected
                 raise HTTPError(500, reason=str(exc))
-            if file_exists:
-                msg = f"file updated by Data Discovery Engine {time.time()}"
-                path = file_name
-                contents = repo.get_contents(path)
-                file_updated = repo.update_file(path, msg, content_encoded, contents.sha, branch='master')
+            if file:
+                file_updated = repo.update_file(file_path, msg, content_encoded, file.sha)
                 if file_updated:
                     logging.info(msg="File updated name "+file_name)
                     self.finish({
                         'success': True,
-                        'msg': f"{file_name} was updated at {time.time()}"
+                        'msg': repo.full_name
                     })
                 else:
                     self.finish({
-                        'success': True,
-                        'msg': f"{file_name} could not be updated"
+                        'success': False,
+                        'msg': repo.full_name
                     })
         else:
             self.finish({
                 'success': False,
-                'msg': f"{repo_name} does not exist"
+                'msg': f"{repo_name} repo or file must exist."
             })
