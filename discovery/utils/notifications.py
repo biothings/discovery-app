c04c34b55669a1a5b33a535ca66ffc59fd3085ed
Jerry <xzhou@scripps.edu>
2020-11-07 22:38:12 -0800

    Add notification dispatch system

--- discovery/utils/notifications.py
+++ discovery/utils/notifications.py
@@ -1,171 +1,245 @@
-import json
-import logging
-import re
+"""
+    API service event use each service's "Notifier"
+    to generate "Message" to be sent through "Channel"s.
+    discovery.web.notify works with this module.
+"""
+
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from pprint import pformat
 
 import requests
-from tornado.httpclient import AsyncHTTPClient, HTTPRequest
+
+
+class N3CProfile():
+    pass
 
 
 class Message(dict):
+    """
+    Logical document that can be sent through services.
+    Processable fields: title, body, url, url_text, image, image_altext
+    Optionally define default field values below.
+    """
+    DEFAULTS = {
+        "title": "Notification Message",
+        "url_text": "View Details",
+        "image_altext": "<IMAGE>"
+    }
 
     def __getattr__(self, attr):
         # virtual attributes
-        if attr in ('title', 'summary', 'body', 'url', 'url_text', 'image', 'image_altext'):
+        if attr in ('title', 'body', 'url', 'url_text', 'image', 'image_altext'):
             if attr in self:
                 return self[attr]
+            if attr in self.DEFAULTS:
+                return self.DEFAULTS[attr]
             return ""
         raise AttributeError()
 
-
-# -------------
-#   helpers
-# -------------
-
-
-def change_link_markdown(description):
-    """
-        Change markdown styling of links to match fit Slack Markdown styling
-        Description text links formatted as [link name](URL), we want <URL|link name>
-    """
-    return re.sub(r'\[(?P<label>[^\[\]]+)\]\((?P<url>[^()]+)\)', r'<\g<url>|\g<label>>', description)
-
-
-def generate_ADF(message):
-    """ https://developer.atlassian.com/cloud/jira/platform/apis/document/playground/ """
-    assert isinstance(message, Message)
-    return {
-        "version": 1,
-        "type": "doc",
-        "content": [
-            {
-                "type": "heading",
-                "attrs": {"level": 2},
-                "content": [{"type": "text", "text": message.title}]
-            },
-            {
+    def to_ADF(self):
+        """
+        Generate ADF for Atlassian Jira payload. Overwrite this to build differently.
+        https://developer.atlassian.com/cloud/jira/platform/apis/document/playground/
+        """
+        adf = {
+            "version": 1,
+            "type": "doc",
+            "content": []
+        }
+        if self.body:
+            adf["content"].append({
                 "type": "paragraph",
-                "content": [{"type": "text", "text": message.body}]
-            },
-            {
+                "content": [{"type": "text", "text": self.body}]
+            })
+        if self.url:
+            adf["content"].append({
                 "type": "paragraph",
-                "content": [{"type": "text", "text": message.url_text,
-                             "marks": [{"type": "link", "attrs": {"href": message.url}}]}]
-            }
-        ]
-    }
+                "content": [{
+                    "type": "text", "text": self.url_text,
+                    "marks": [{"type": "link", "attrs": {"href": self.url}}]
+                }]
+            })
+        return adf
 
-
-def generate_slack_params(message):
-    """
-    Generate parameters that will be used in slack post request.
-    In this case, markdown is used to generate formatting that
-    will show in Slack message
-    """
-    assert isinstance(message, Message)
-
-    return {
-        "attachments": [{
-            "blocks": [
-                {
-                    "type": "header",
-                    "text": {"type": "plain_text", "text": ":sparkles: " + message.title, "emoji": True}
-                },
-                {
-                    "type": "divider"
-                },
-                {
-                    "type": "section",
-                    "text": {"type": "mrkdwn", "text": f"*<{message.url}|{message.url_text}>*"}
-                },
-                {
-                    "type": "section",
-                    "text": {"type": "mrkdwn", "text": message.body},
-                    "accessory": {"type": "image", "image_url": message.url, "alt_text": message.url_alt}
-                },
-                {
-                    "type": "section",
-                    "text": {"type": "mrkdwn", "text": message.url_text},
-                    "accessory": {
-                        "type": "button",
-                        "text": {"type": "plain_text", "text": message.url_text, "emoji": True},
-                        "value": message.url_text,
-                        "url": message.url,
-                        "action_id": "button-action"
-                    }
+    def to_slack_payload(self):
+        """
+        Generate slack webhook notification payload.
+        https://api.slack.com/messaging/composing/layouts
+        """
+        blocks = []
+        if self.title:
+            blocks.append({
+                "type": "header",
+                "text": {
+                    "type": "plain_text",
+                    "text": ":sparkles: " + self.title,
+                    "emoji": True
                 }
-            ]
-        }]
-    }
+            })
+            blocks.append({
+                "type": "divider"
+            })
+        if self.body:
+            body = {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": self.body},
+            }
+            if self.image:
+                body["accessory"] = {
+                    "type": "image",
+                    "image_url": self.image,
+                    "alt_text": self.image_altext
+                }
+            blocks.append(body)
+        if self.url:
+            blocks.append({
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"*<{self.url}|{self.url_text}>*"}
+            })
+        return {
+            "attachments": [{
+                "blocks": blocks
+            }]
+        }
 
+    def to_jira_payload(self, profile):
+        """
+        Combine notification message with project profile to
+        genereate jira issue tracking ticket request payload.
+        """
+        return {
+            "fields": {
+                "project": {"id": profile.project_id},
+                "summary": self.title,
+                "issuetype": {"id": profile.issuetype_id},
+                "assignee": {"id": profile.assignee_id},
+                "reporter": {"id": profile.reporter_id},
+                "priority": {"id": "3"},
+                "labels": [profile.label],
+                "description": self.to_ADF()
+            }
+        }
 
-def generate_mail(sendfrom, sendto, subject, message, alt_text=""):
-    # Ref: https://docs.aws.amazon.com/ses/latest/DeveloperGuide/examples-send-using-smtp.html
+    def to_email_payload(self, sendfrom, sendto):
+        """
+        Build a MIMEMultipart message that can be sent as an email.
+        https://docs.aws.amazon.com/ses/latest/DeveloperGuide/examples-send-using-smtp.html
+        """
+        # Create message container - the correct MIME type is multipart/alternative.
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = self.title
+        # msg['From'] = email.utils.formataddr((SENDERNAME, SENDER))
+        msg['From'] = sendfrom
+        msg['To'] = sendto
 
-    # Create message container - the correct MIME type is multipart/alternative.
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = subject
-    # msg['From'] = email.utils.formataddr((SENDERNAME, SENDER))
-    msg['From'] = sendfrom
-    msg['To'] = sendto
-    # Comment or delete the next line if you are not using a configuration set
-    # msg.add_header('X-SES-CONFIGURATION-SET',CONFIGURATION_SET)
+        # Comment or delete the next line if you are not using a configuration set
+        # msg.add_header('X-SES-CONFIGURATION-SET',CONFIGURATION_SET)
 
-    # Record the MIME types of both parts - text/plain and text/html.
-    part1 = MIMEText(alt_text, 'plain')
-    part2 = MIMEText(message, 'html')
+        # Record the MIME types of both parts - text/plain and text/html.
+        part1 = MIMEText(self.body, 'plain')
+        part2 = MIMEText(f"""
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>{self.title}</title>
+            </head>
+            <body>
+                <p>{self.body}</p>
+            </body>
+        </html>""", 'html')
 
-    # Attach parts into message container.
-    # According to RFC 2046, the last part of a multipart message, in this case
-    # the HTML message, is best and preferred.
-    msg.attach(part1)
-    msg.attach(part2)
+        # Attach parts into message container.
+        # According to RFC 2046, the last part of a multipart message, in this case
+        # the HTML message, is best and preferred.
+        msg.attach(part1)
+        msg.attach(part2)
 
-    return msg
+        return msg
+
 
 # -------------
-#   senders
+#   Senders
 # -------------
 
 
-def send_mail(sendfrom, sendto, subject, message, host, port, user, password, alt_text=""):
-    """A utility function to send an html email.
-       message should be a HTML string, alt_text can be provided
-       as a text-only version.
+def send_mail(sendfrom, sendto, message, host, port, user, password):
     """
-    msg = generate_mail(sendfrom, sendto, subject, message, alt_text)
-    # Try to send the message.
-    try:
-        server = smtplib.SMTP(host, port)
-        server.ehlo()
-        server.starttls()
-        # stmplib docs recommend calling ehlo() before & after starttls()
-        server.ehlo()
-        server.login(user, password)
-        server.sendmail(msg['From'], msg['To'], msg.as_string())
-        server.close()
-    # Display an error message if something goes wrong.
-    except Exception as e:
-        logging.error(str(e))
-    else:
-        logging.info("email sent.")
+        A utility function to send an html email.
+        message should be a HTML string.
+    """
+    msg = message.to_email_payload(sendfrom, sendto)
+    server = smtplib.SMTP(host, port)
+    server.ehlo()
+    server.starttls()
+    # stmplib docs recommend calling ehlo() before & after starttls()
+    server.ehlo()
+    server.login(user, password)
+    server.sendmail(msg['From'], msg['To'], msg.as_string())
+    server.close()
 
 
-def send_slack_msg(web_hooks, data, res, github_user, portal=None):
-    """
-    Make requests to slack to post information about newly registered API.
-    Notifications will be sent to every
-    channel/webhook that is not tag specific, or will be sent to
-    slack if the registered API contains a tag that is also specific
-    a channel/webhook.
-    """
-    # headers = {'content-type': 'application/json'}
-    # http_client = AsyncHTTPClient()
-    # for wh in web_hooks:
-    #     params = generate_slack_params(data, res, github_user, portal)
-    #     req = HTTPRequest(url=wh, method='POST', body=json.dumps(params), headers=headers)
-    #     http_client = AsyncHTTPClient()
-    #     http_client.fetch(req)
+def send_slack_msg(web_hook, message):
+    return requests.post(web_hook, json=message.to_slack_payload())
+
+
+def send_n3c_msg(user, password, profile, message):
+    return requests.post(
+        url="https://n3c-help.atlassian.net/rest/api/3/issue",
+        auth=(user, password),
+        json=message.to_jira_payload(profile)
+    )
+
+# -------------
+#    Tests
+# -------------
+
+
+test_message = Message({
+    "title": (
+        "From its medieval origins to the digital era, "
+        "learn everything there is to know about the ubiquitous lorem ipsum passage."
+    ),
+    "body": "\n\n".join((
+        ("Paragraph 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, "
+         "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."),
+        ("Paragraph 2 Ut enim ad minim veniam, quis nostrud  ullamco laboris "
+         "nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in "
+         "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla."
+         "Excepteur sint occaecat cupidatat non proide, molt anim est laborum.")
+    )),
+    "url": "http://example.com/",
+    "url_text": "Click to learn more.",
+    "image": "https://biothings.io/static/img/biothings-text-2.png",
+    "image_altext": "BIOTHINGS_API_LOGO"
+})
+
+
+def test_slack():
+
+    response = send_slack_msg(input("slack webhook url: "), test_message)
+    print(response.status_code)
+    print(response.text)
+
+
+def test_n3c_dataset():
+
+    dde = N3CProfile()
+    dde.project_id = "10001"
+    dde.issuetype_id = "10014"
+    dde.assignee_id = "557058:3b14bc92-4371-460c-8b25-b7a44db23e26"
+    dde.reporter_id = "557058:3b14bc92-4371-460c-8b25-b7a44db23e26"
+    dde.label = "DATASET"
+    response = send_n3c_msg(
+        input("user: "),
+        input("password: "),
+        dde, test_message)
+    print(response.status_code)
+    print(response.text)
+
+
+if __name__ == '__main__':
+    test_slack()
+    test_n3c_dataset()
--- discovery/web/notify.py
+++ discovery/web/notify.py
@@ -1,61 +1,41 @@
 import json
 import logging
 import re
+from types import SimpleNamespace
+from pyadf.document import Document as ADF
 
 from discovery.utils import notifications
-from tornado.httpclient import HTTPRequest
-
-
-def get_portal_image(guide):
-
-    for portal, url in {
-            "outbreak": "https://discovery.biothings.io/static/img/outbreak.png",
-            "niaid": "https://discovery.biothings.io/static/img/niaid.png",
-            "n3c": "https://discovery.biothings.io/static/img/N3Co.png",
-            "": "https://discovery.biothings.io/static/img/dde-logo-o.png"  # default
-    }.items():
-        if portal in guide:
-            return url
+from tornado.httpclient import HTTPRequest, HTTPClient
 
 
 class Channel:
 
     def send(self, message):
-        """ return an interable object of http requests to make. """
         raise NotImplementedError()
 
 
 class EmailChannel(Channel):
-    pass
+
+    def send(self, message):
+        yield message.to_email_payload('from', 'to')  # TODO
 
 
 class N3CChannel(Channel):
 
-    def __init__(self, auth_header, profile):
-        self.auth_header = auth_header
-        self.profile = profile
+    def __init__(self, user, password, profile):
+        self.user = user
+        self.password = password
+        self.profile = profile  # project related info
 
     def send(self, message):
         yield HTTPRequest(
             url="https://n3c-help.atlassian.net/rest/api/3/issue",
             method="POST",
-            headers={
-                "Authorization": self.auth_header,
-                "Content-Type": "application/json"
-            },
-            body={
-                "fields": {
-                    "project": {"id": self.profile.project_id},
-                    "summary": message.summary,
-                    "issuetype": {"id": self.profile.issuetype_id},
-                    "assignee": {"id": self.profile.assignee_id},
-                    "reporter": {"id": self.profile.reporter_id},
-                    "priority": {"id": "3"},
-                    "labels": [self.profile.label],
-                    "description": notifications.generate_ADF(message)
-                }
-            }
-        ),
+            headers={"Content-Type": "application/json"},
+            auth_username=self.user,
+            auth_password=self.password,
+            body=json.dumps(message.to_jira_payload(self.profile))
+        )
 
 
 class SlackChannel(Channel):
@@ -69,11 +49,16 @@ class SlackChannel(Channel):
                 url=url,
                 method='POST',
                 headers={'content-type': 'application/json'},
-                body=json.dumps(notifications.generate_slack_params(message))
+                body=json.dumps(message.to_slack_payload())
             )
 
 
 class Notifier:
+    """
+    Configurable message channel manager.
+    Define an application's notification presets.
+    Each attribute method corresponds to a servie event.
+    """
 
     def __init__(self):
         self.configured = False
@@ -83,19 +68,12 @@ class Notifier:
 
         if not self.configured:
 
-            if hasattr(settings, 'SLACK_WEB_HOOKS'):
-                self.channels.append(SlackChannel(getattr(settings, 'SLACK_WEB_HOOKS')))
-
-            if hasattr(settings, 'N3C_AUTH_HEADER'):
-                profile = object()
-                profile.project_id = "10001"
-                profile.issuetype_id = "10008"
-                profile.assignee_id = "557058:3b14bc92-4371-460c-8b25-b7a44db23e26"
-                profile.reporter_id = "557058:3b14bc92-4371-460c-8b25-b7a44db23e26"
-                profile.label = "DATASET"
-                self.channels.append(N3CChannel(getattr(settings, 'N3C_AUTH_HEADER'), profile))
+            if hasattr(settings, 'SLACK_WEBHOOKS'):
+                self.channels.append(SlackChannel(getattr(settings, 'SLACK_WEBHOOKS')))
 
             # ------------ email ------------
+            #
+            # -------------------------------
 
             self.configured = True
 
@@ -109,30 +87,176 @@ class Notifier:
                 yield request
 
 
+class DatasetMessage(notifications.Message):
+    """
+    Dataset Registry Notifications
+    Additionally aware of _id, doc, and meta field.
+    """
+
+    def to_ADF(self):
+        """
+        Attach dataset metadata details at the end of the message.
+        Use pyadf to build complex document structures. See documentation:
+        https://developer.atlassian.com/cloud/jira/platform/apis/document/libs/
+        """
+        adf = super().to_ADF()
+        if 'doc' in self:
+            adf["content"].append(ADF().heading(3).text("Overview").to_doc())
+            doc = ADF().bullet_list()
+            doc.add_item(ADF().paragraph().text("Name: ").text(self["doc"].get("name")))
+            doc.add_item(ADF().paragraph().text("URL: ").text(self["doc"].get("url")).link(self["doc"].get("url")))
+            adf["content"].append(doc.to_doc())
+            adf["content"].append(ADF().heading(3).text("Raw").to_doc())
+            adf["content"].append(ADF().codeblock("json").text(json.dumps(self["doc"], indent=4)).to_doc())
+        return adf
+
+
 class DatasetNotifier(Notifier):
+    """
+    Dataset Registry Notifier
+    Additionally support N3C Channel
+    """
+
+    def configure(self, settings):
+
+        if not self.configured:
+            super().configure(settings)
+
+            if hasattr(settings, 'N3C_AUTH_USER') and \
+                    hasattr(settings, 'N3C_AUTH_PASSWORD'):
+                profile = SimpleNamespace()
+                profile.project_id = "10001"
+                profile.issuetype_id = "10014"
+                profile.assignee_id = "557058:3b14bc92-4371-460c-8b25-b7a44db23e26"
+                profile.reporter_id = "557058:3b14bc92-4371-460c-8b25-b7a44db23e26"
+                profile.label = "DATASET"
+                self.channels.append(N3CChannel(
+                    getattr(settings, 'N3C_AUTH_USER'),
+                    getattr(settings, 'N3C_AUTH_PASSWORD'),
+                    profile))
+
+    @staticmethod
+    def get_portal_image(guide=""):
+
+        for portal, url in {
+                "outbreak": "https://discovery.biothings.io/static/img/outbreak.png",
+                "niaid": "https://discovery.biothings.io/static/img/niaid.png",
+                "n3c": "https://discovery.biothings.io/static/img/N3Co.png",
+                "": "https://discovery.biothings.io/static/img/dde-logo-o.png"  # default
+        }.items():
+            if portal in guide:
+                return url
 
     def add(self, _id, doc, user, **meta):
 
-        message = notifications.Message({
-            "title": f"External Dataset Request - {doc.get('identifier')}",
-            "summary": f"New metadata {doc.get('name')} has been registered on Data Discovery Engine",
-            "body": "\n".join((f"{key}:{val}" for key, val in {
-                "user": user,
-                "identifier": doc.get('identifier'),
-                "name": doc.get('name'),
-                "description": doc.get('description')
-            }.items())),
+        message = DatasetMessage({
+            "title": "External Dataset Request",
+            "body": f'A new dataset "{doc.get("name")}" has been submitted by {user} on Data Discovery Engine.',
             "url": f"http://discovery.biothings.io/dataset/{_id}",
             "url_text": "View details about this dataset",
-            "image": get_portal_image(meta.get('guide', '')),
-            "image_altext": "DDE_PORTAL_IMG"
+            "image": DatasetNotifier.get_portal_image(meta.get('guide', '')),
+            "image_altext": "DDE_PORTAL_IMG",
+            "doc": doc  # produce additional jira ticket content
+        })
+
+        for channel in self.channels:
+
+            if isinstance(channel, N3CChannel):
+                if meta.get('schema') != 'n3c:Dataset':
+                    continue
+
+            for request in channel.send(message):
+                yield request
+
+    def delete(self, _id, name, user):
+
+        message = notifications.Message({
+            "title": "Dataset Metadata Deleted",
+            "body": f'Dataset "{name}" is deleted by {user}.',
         })
         return self.broadcast(message)
 
+    def update(self, _id, name, version, user):
+
+        message = notifications.Message({
+            "title": "Dataset Metadata Updated",
+            "body": f'Dataset {name} is updated by {user}. Current version is {version}.',
+            "url": f"http://discovery.biothings.io/dataset/{_id}",
+            "url_text": "View details",
+        })
+        return self.broadcast(message)
+
+    def broadcast(self, message):
+        for channel in self.channels:
+            if isinstance(channel, N3CChannel):
+                continue  # skip N3C by default
+            for request in channel.send(message):
+                yield request
+
 
 class SchemaNotifier(Notifier):
-    pass
+
+    def add(self, namespace, num_classes):
+        return self.broadcast(notifications.Message({
+            "title": "New Schema Registration",
+            "body": f'{num_classes} classes have been registered under namespace "{namespace}".',
+            "url": f"https://discovery.biothings.io/view/{namespace}",
+            "url_text": "Visualize Schema"
+        }))
+
+    def delete(self, namespace, num_classes):
+        return self.broadcast(notifications.Message({
+            "title": "Schema Deleted",
+            "body": f'{num_classes} classes have been deleted under namespace "{namespace}".'
+        }))
+
+    def update(self, namespace, num_classes):
+        return self.broadcast(notifications.Message({
+            "title": "Schema Updated",
+            "body": f'Schema "{namespace}" updated. {num_classes} current classes.',
+            "url": f"https://discovery.biothings.io/view/{namespace}",
+            "url_text": "Visualize Schema"
+        }))
 
 
 dataset = DatasetNotifier()
 schema = SchemaNotifier()
+
+
+def test_on(requests):
+    client = HTTPClient()
+    for request in requests:
+        response = client.fetch(request)
+        print(response.body)
+
+
+def test_schema():
+    settings = SimpleNamespace()
+    settings.SLACK_WEBHOOKS = [input("slack webhook url: ")]
+    schema.configure(settings)
+    test_on(schema.add("ec", 538))
+    test_on(schema.update("ec", 270))
+    test_on(schema.delete("ec", 270))
+
+
+def test_dataset():
+    settings = SimpleNamespace()
+    settings.SLACK_WEBHOOKS = [input("slack webhook url: ")]
+    settings.N3C_AUTH_USER = input("n3c user: ")
+    settings.N3C_AUTH_PASSWORD = input("n3c password: ")
+    dataset.configure(settings)
+    test_on(dataset.add("0x0000", {
+        "identifier": "grandtest",
+        "description": "learn everything there is to know.",
+        "name": "lorem ipsum",
+        "url": "http://example.com/"
+    }, "wethepeople", **{
+        "schema": "n3c:Dataset",
+    }))
+    test_on(dataset.update("0x0000", "lorem ipsum", "v2", "wethepeople"))
+    test_on(dataset.delete("0x0000", "lorem ipsum", "wethepeople"))
+
+
+if __name__ == '__main__':
+    test_schema()
+    test_dataset()
