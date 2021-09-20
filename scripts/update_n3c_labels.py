e79de6213e4349b934a4e81ec32edb7e2e91565e
Marco Cano <artofmarco@gmail.com>
2021-09-20 14:39:58 -0700

    add scripts to update labels, ts. remove frontend check for old labels

---
+++ scripts/update_n3c_labels.py
@@ -0,0 +1,34 @@
+
+import logging
+from discovery.data.dataset import Dataset
+
+def updateLabels():
+    '''
+        Update dataset submission status label
+    '''
+    docs = Dataset.search()
+    for doc in docs.scan():
+        status = getattr( getattr(doc, "_n3c", None), 'status', None)
+        if status:
+            if status == 'Done/Imported':
+                doc.update(**{'_n3c':{'status': 'Available'}})
+                logging.info(f"Doc  updating label (1)")
+            elif status == 'Done/Rejected':
+                doc.update(**{'_n3c':{'status': 'Denied'}})
+                logging.info(f"Doc  updating label (2)")
+            elif status == 'Ready for Import':
+                doc.update(**{'_n3c':{'status': 'Approved for Import'}})
+                logging.info(f"Doc  updating label (3)")
+            elif status == 'Backglog':
+                doc.update(**{'_n3c':{'status': 'Pending Review'}})
+                logging.info(f"Doc  updating label (4)")
+            elif status == 'In Review':
+                doc.update(**{'_n3c':{'status': 'Pending Review'}})
+                logging.info(f"Doc  updating label (5)")
+            else:
+                logging.info(f"Doc  n3c label already up to date.")
+
+
+if __name__ == "__main__":
+    updateLabels()
+
