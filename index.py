4c3987bc498ef9e72cddcb54417ee3f902982d94
Chunlei Wu <anewgene@yahoo.com>
2022-09-14 22:25:10 -0700

    feat: use filelock to ensure only one process running scheduled jobs

--- index.py
+++ index.py
@@ -1,8 +1,10 @@
 ''' Tornado Web Server Starting Script - Application Entry Point '''
 
 from threading import Thread
+import logging
 
 from aiocron import crontab
+from filelock import FileLock, Timeout
 from biothings.web.launcher import main
 
 from discovery.handlers import HANDLERS, TemplateHandler
@@ -10,9 +12,26 @@ from discovery.notify import update_n3c_routine
 from discovery.utils.backup import daily_backup_routine
 
 
+# Create a lock can only be acquired by one process.
+# Make sure only one process should perform backup routines
+_lock = FileLock(".lock", timeout=0)
+try:  # it will be released upon exit
+    _lock.acquire()
+except Timeout:
+    pass
+
 def routine():
-    update_n3c_routine()
-    daily_backup_routine()
+    logger = logging.getLogger("routine")
+    try:
+        # if previously acquired, it won't block here
+        _lock.acquire()
+    except Timeout:
+        logger.warning("No need to run scheduled routine jobs in this process.")
+    else:
+        logger.info("update_n3c_routine()")
+        update_n3c_routine()
+        logger.info("daily_backup_routine()")
+        daily_backup_routine()
 
 
 def run_routine():
