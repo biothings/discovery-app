5af7487d058ed142f4bc4fe5c2246216f5451804
Chunlei Wu <anewgene@yahoo.com>
2022-09-22 22:01:33 -0700

    style: fourmat fixes

--- tests/test_schema_registry.py
+++ tests/test_schema_registry.py
@@ -1,46 +1,43 @@
-'''
+"""
     Registry Handler Tester
-'''
+"""
 
 import pytest
+
 from discovery.registry import schemas
 from discovery.utils import indices
+
 from .test_base import DiscoveryTestCase
 
-BTS_URL = 'https://raw.githubusercontent.com/data2health/schemas/biothings/biothings/biothings_curie.jsonld'
+BTS_URL = "https://raw.githubusercontent.com/data2health/schemas/biothings/biothings/biothings_curie.jsonld"
 
 
 @pytest.fixture(scope="module", autouse=True)
 def setup():
-    if not schemas.exists('bts'):
-        schemas.add(
-            namespace='bts',
-            url=BTS_URL,
-            user='minions@example.com'
-        )
+    if not schemas.exists("bts"):
+        schemas.add(namespace="bts", url=BTS_URL, user="minions@example.com")
 
 
 class DiscoveryAPITest(DiscoveryTestCase):
-
     def refresh(self):
         indices.refresh()
 
     def test_00_head(self):
-        self.request('registry/bts', method='HEAD')
+        self.request("registry/bts", method="HEAD")
 
     def test_01_head(self):
-        self.request('registry/does_not_exist', method='HEAD', expect=404)
+        self.request("registry/does_not_exist", method="HEAD", expect=404)
 
     def test_10_get(self):
-        res = self.request('registry?user=minions@example.com').json()
-        for hit in res['hits']:
-            if hit['namespace'] == 'bts':
+        res = self.request("registry?user=minions@example.com").json()
+        for hit in res["hits"]:
+            if hit["namespace"] == "bts":
                 break
         else:  # bts schema not found
             assert False
 
     def test_11_get(self):
-        ''' GET /registry/<prefix>
+        """GET /registry/<prefix>
         {
             "name": "bts",
             "url": "https://raw.githubusercontent.com/data2health/schemas/biothings/biothings/biothings_curie.jsonld",
@@ -59,17 +56,17 @@ class DiscoveryAPITest(DiscoveryTestCase):
             },
             "hits": [ ... ] // 70 items
         }
-        '''
-        res = self.request('registry/bts').json()
-        assert res['name'] == 'bts'
-        assert res['url'] == BTS_URL
-        assert res['source']
-        assert res['total']
+        """
+        res = self.request("registry/bts").json()
+        assert res["name"] == "bts"
+        assert res["url"] == BTS_URL
+        assert res["source"]
+        assert res["total"]
         # assert res['context'] # TODO
-        assert res['hits']
+        assert res["hits"]
 
     def test_12_get(self):
-        ''' GET /registry/<prefix>/<label>
+        """GET /registry/<prefix>/<label>
         {
             "namespace": "bts",
             "name": "bts:BiologicalEntity",
@@ -80,38 +77,35 @@ class DiscoveryAPITest(DiscoveryTestCase):
             "properties": [ ... ],
             "ref": true
         }
-        '''
-        res = self.request('registry/bts/bts:BiologicalEntity').json()
-        assert res['label'] == 'BiologicalEntity'
-        assert res['prefix'] == 'bts'
+        """
+        res = self.request("registry/bts/bts:BiologicalEntity").json()
+        assert res["label"] == "BiologicalEntity"
+        assert res["prefix"] == "bts"
 
     def test_20_delete(self):
-        self.request('registry/bts', method='DELETE', expect=401)
+        self.request("registry/bts", method="DELETE", expect=401)
 
     def test_21_delete(self):
-        self.request('registry/bts', method='DELETE', headers=self.evil_user, expect=403)
+        self.request("registry/bts", method="DELETE", headers=self.evil_user, expect=403)
 
     def test_22_delete(self):
-        self.request('registry/xtx', method='DELETE', headers=self.auth_user, expect=404)
+        self.request("registry/xtx", method="DELETE", headers=self.auth_user, expect=404)
 
     def test_23_delete(self):
-        self.query(q='BiologicalEntity', hits=True)
-        self.request('registry/bts', expect=200)
-        self.request('registry/bts', method='DELETE', headers=self.auth_user)
-        self.request('registry/bts', expect=404)
+        self.query(q="BiologicalEntity", hits=True)
+        self.request("registry/bts", expect=200)
+        self.request("registry/bts", method="DELETE", headers=self.auth_user)
+        self.request("registry/bts", expect=404)
         self.refresh()
-        self.query(q='BiologicalEntity', hits=False)
+        self.query(q="BiologicalEntity", hits=False)
 
     def test_30_post(self):
-        if schemas.exists('bts'):
-            schemas.delete('bts')
-        doc = {
-            'url': BTS_URL,
-            'namespace': 'bts'
-        }
-        self.query(q='BiologicalEntity', hits=False)
-        self.request('registry/bts', expect=404)
-        self.request('registry', method='POST', json=doc, headers=self.auth_user)
-        self.request('registry/bts', expect=200)
+        if schemas.exists("bts"):
+            schemas.delete("bts")
+        doc = {"url": BTS_URL, "namespace": "bts"}
+        self.query(q="BiologicalEntity", hits=False)
+        self.request("registry/bts", expect=404)
+        self.request("registry", method="POST", json=doc, headers=self.auth_user)
+        self.request("registry/bts", expect=200)
         self.refresh()
-        self.query(q='BiologicalEntity')
+        self.query(q="BiologicalEntity")
--- tests/test_schema_viewer.py
+++ tests/test_schema_viewer.py
@@ -2,9 +2,8 @@ from biothings.tests.web import BiothingsTestCase
 
 
 class TestSchemaViewer(BiothingsTestCase):
-
     def test_01(self):
-        """ GET /api/view?url=<CTSA_Dataset_URL>
+        """GET /api/view?url=<CTSA_Dataset_URL>
         {
             "total": 6,
             "context": {
@@ -28,57 +27,57 @@ class TestSchemaViewer(BiothingsTestCase):
             ]
         }
         """
-        url = 'https://raw.githubusercontent.com/biothings/discovery-app/master/tests/test_schema/CTSADataset.json'
-        res = self.request('/api/view?url=' + url).json()
-        assert res['total']
-        assert res['context']
-        assert res['hits'][0]['label'] == 'CTSADataset'
-        assert res['hits'][0]['properties'][0]['curie'] == 'bts:samples'
+        url = "https://raw.githubusercontent.com/biothings/discovery-app/master/tests/test_schema/CTSADataset.json"
+        res = self.request("/api/view?url=" + url).json()
+        assert res["total"]
+        assert res["context"]
+        assert res["hits"][0]["label"] == "CTSADataset"
+        assert res["hits"][0]["properties"][0]["curie"] == "bts:samples"
 
     def test_02(self):
-        """ Fail Validation
+        """Fail Validation
         {
             "code": 400,
             "success": false,
             "error": "field doi in $validation is not correctly documented"
         }
         """
-        url = 'https://raw.githubusercontent.com/biothings/discovery-app/master/tests/test_schema/dataset_invalid_1.jsonld'
-        res = self.request('/api/view?url=' + url, expect=400).json()
-        assert res['error'] == 'field doi in $validation is not correctly documented'
+        url = "https://raw.githubusercontent.com/biothings/discovery-app/master/tests/test_schema/dataset_invalid_1.jsonld"
+        res = self.request("/api/view?url=" + url, expect=400).json()
+        assert res["error"] == "field doi in $validation is not correctly documented"
 
     def test_03(self):
-        """ Invalid Json
+        """Invalid Json
         {
             "code": 400,
             "success": false,
             "error": "Expecting property name enclosed in double quotes: line 2 column 3 (char 4)"
         }
         """
-        url = 'https://raw.githubusercontent.com/biothings/discovery-app/master/tests/test_schema/dataset_invalid_2.jsonld'
-        res = self.request('/api/view?url=' + url, expect=400).json()
-        assert 'error' in res
+        url = "https://raw.githubusercontent.com/biothings/discovery-app/master/tests/test_schema/dataset_invalid_2.jsonld"
+        res = self.request("/api/view?url=" + url, expect=400).json()
+        assert "error" in res
 
     def test_04(self):
-        """ Invalid URL 1
+        """Invalid URL 1
         {
             "code": 400,
             "success": false,
             "error": "HTTP 404: Not Found"
         }
         """
-        url = 'https://example.com/dataset_invalid_2.jsonld'
-        res = self.request('/api/view?url=' + url, expect=400).json()
-        assert 'error' in res
+        url = "https://example.com/dataset_invalid_2.jsonld"
+        res = self.request("/api/view?url=" + url, expect=400).json()
+        assert "error" in res
 
     def test_05(self):
-        """ Invalid URL 2
+        """Invalid URL 2
         {
             "code": 400,
             "success": false,
             "error": "HTTP 599: Could not resolve host: INVALID"
         }
         """
-        url = 'INVALID'
-        res = self.request('/api/view?url=' + url, expect=400).json()
-        assert 'error' in res
+        url = "INVALID"
+        res = self.request("/api/view?url=" + url, expect=400).json()
+        assert "error" in res
