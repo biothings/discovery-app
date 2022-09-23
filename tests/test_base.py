ae727369f341f085471c0b93a547add587ccd6b3
Chunlei Wu <anewgene@yahoo.com>
2022-09-22 22:01:33 -0700

    style: fourmat fixes

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,28 +1,26 @@
+from biothings.tests.web import BiothingsTestCase
 from tornado.escape import json_encode
 from tornado.web import create_signed_value
 
-from biothings.tests.web import BiothingsTestCase
-
 
 class DiscoveryTestCase(BiothingsTestCase):
-
     @classmethod
     def cookie_header(cls, username):
-        cookie_name, cookie_value = 'user', {'login': username}
+        cookie_name, cookie_value = "user", {"login": username}
         secure_cookie = create_signed_value(
-            cls.settings.COOKIE_SECRET, cookie_name,
-            json_encode(cookie_value))
-        return {'Cookie': '='.join((cookie_name, secure_cookie.decode()))}
+            cls.settings.COOKIE_SECRET, cookie_name, json_encode(cookie_value)
+        )
+        return {"Cookie": "=".join((cookie_name, secure_cookie.decode()))}
 
     @property
     def auth_user(self):
-        return self.cookie_header('minions@example.com')
+        return self.cookie_header("minions@example.com")
 
     @property
     def evil_user(self):
-        return self.cookie_header('villain@example.com')
+        return self.cookie_header("villain@example.com")
 
     def get_app(self):
         app = super().get_app()
-        app.settings['debug'] = True
+        app.settings["debug"] = True
         return app
--- tests/test_schema_query.py
+++ tests/test_schema_query.py
@@ -1,38 +1,37 @@
-'''
+"""
     Biothings ESQueryHandler Type Tester
-'''
+"""
 
 from biothings.tests.web import BiothingsTestCase
 
 
 class DiscoveryQueryTest(BiothingsTestCase):
-
     def test_01_default(self):
-        '''
+        """
         [QUERY] Basic functionality
-        '''
-        res = self.query(q='__all__')
-        assert res['total'] > 770
+        """
+        res = self.query(q="__all__")
+        assert res["total"] > 770
 
     def test_02_customization(self):
-        '''
+        """
         [QUERY] Customization by name
-        '''
-        self.query(q='schema:Library')
-        self.query(q='library')  # case-insensitivity
-        self.query(q='lib')  # preifx match
+        """
+        self.query(q="schema:Library")
+        self.query(q="library")  # case-insensitivity
+        self.query(q="lib")  # preifx match
 
     def test_03_customization(self):
-        '''
+        """
         [QUERY] Customization by properties
-        '''
-        self.query(q='activityFrequency')
+        """
+        self.query(q="activityFrequency")
 
     def test_04_customization(self):
-        '''
+        """
         [QUERY] Customization by parents
-        '''
-        res = self.query(q='schema:Intangible')
-        assert res['total'] >= 3
-        res = self.query(q='Thing')
-        assert res['total'] > 770
+        """
+        res = self.query(q="schema:Intangible")
+        assert res["total"] >= 3
+        res = self.query(q="Thing")
+        assert res["total"] > 770
