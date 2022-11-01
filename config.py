ce6d14d7682aaf4981903075a1a11a19206042ed
Marco Cano <artofmarco@gmail.com>
2022-11-01 11:14:33 -0700

    fix: :hammer: update endpoint name

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ APP_LIST = [
     (r"/api/view/?", "discovery.handlers.api.SchemaViewHandler"),
     (r"/api/gh/([^/]+)/?", "discovery.handlers.api.GHHandler"),
     (r"/api/gh/?", "discovery.handlers.api.GHHandler"),
-    (r"/api/validate/([^/]+)/([^/]+)/?", "discovery.handlers.api.MetadataValidationHandler"),
+    (r"/api/schema/validate/([^/]+)/([^/]+)/?", "discovery.handlers.api.MetadataValidationHandler"),
 ]
 
 # biothings web tester will read this
