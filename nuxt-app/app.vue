35ae83ede503b65b6490b4889c88f6d457265aaa
Marco Cano <artofmarco@gmail.com>
2023-01-13 17:12:59 -0800

    fix: :hammer: remove depricated font

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -117,10 +117,6 @@ useHead({
       crossorigin: "anonymous",
     },
     {
-      href: "https://fonts.googleapis.com/css?family:Sulphur+Point&display:swap",
-      rel: "stylesheet",
-    },
-    {
       src: "https://code.jquery.com/jquery-3.3.1.slim.min.js",
       integrity:
         "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo",
