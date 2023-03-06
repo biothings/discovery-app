8300103c3988b1c4fadf99aa29b6ef440704d7fb
Nichollette <nichollette.aco@gmail.com>
2023-03-06 15:16:53 -0500

    github action yml

--- discovery/registry/schemas.py
+++ discovery/registry/schemas.py
@@ -452,13 +452,13 @@ def get_all_contexts():
     return {k: v for k, v in contexts.items() if v}
 
 
-def get_refresh_status(namespace):
-    """
-    Get a schemas update status through namespace
-    """
-    schema = ESSchemaFile.get(id=namespace, ignore=404, _source="_status")
+# def get_refresh_status(namespace):
+#     """
+#     Get a schemas update status through namespace
+#     """
+#     schema = ESSchemaFile.get(id=namespace, ignore=404, _source="_status")
 
-    if schema:
-        return RegistryDocument.wraps(schema).meta
+#     if schema:
+#         return RegistryDocument.wraps(schema).meta
 
-    raise NoEntityError(f"schema '{namespace}' does not exist.")
+#     raise NoEntityError(f"schema '{namespace}' does not exist.")
