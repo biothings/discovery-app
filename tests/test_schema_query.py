79714c6fd7aed1ee74a9a15f2b8ca36a1fef1598
Nichollette <nichollette.aco@gmail.com>
2023-04-28 19:44:38 -0400

    testing schema query

---
+++ tests/test_schema_query.py
@@ -0,0 +1,37 @@
+"""
+    Biothings ESQueryHandler Type Tester
+"""
+from biothings.tests.web import BiothingsTestCase
+
+class DiscoveryQueryTest(BiothingsTestCase):
+    TEST_DATA_DIR_NAME = 'schemas'
+
+    def test_01_default(self):
+        """
+        [QUERY] Basic functionality
+        """
+        res = self.query(q="__all__")
+        assert res["total"] > 1
+
+    def test_02_customization(self):
+        """
+        [QUERY] Customization by name
+        """
+        self.query(q="schema:Thing")
+        self.query(q="outbreak:ComputationalTool")
+        # self.query(q="bts")  # preifx match
+
+    def test_03_customization(self):
+        """
+        [QUERY] Customization by properties
+        """
+        self.query(q="alternateName")
+
+    def test_04_customization(self):
+        """
+        [QUERY] Customization by parents
+        """
+        res = self.query(q="schema:Intangible")
+        assert res["total"] >= 1
+        res = self.query(q="Thing")
+        assert res["total"] > 2 # 770
