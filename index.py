1be65e0a0efe2fcbe5535b727a57e5b5a9d64722
everaldorodrigo <everaldorodrigo@gmail.com>
2023-05-25 11:32:07 -0700

    Implement schedule lock for multiprocess.

--- index.py
+++ index.py
@@ -20,19 +20,15 @@ define("proxy_url", default="http://localhost:3000/", help="localhost port servi
 # Create a lock can only be acquired by one process.
 # Make sure only one process should perform backup routines
 _lock = FileLock(".lock", timeout=0)
-try:  # it will be released upon exit
-    _lock.acquire()
-except Timeout:
-    pass
 
 
 def routine():
     logger = logging.getLogger("routine")
     try:
-        # if previously acquired, it won't block here
         _lock.acquire()
+        logger.info("Schedule lock acquired successfully.")
     except Timeout:
-        logger.warning("No need to run scheduled routine jobs in this process.")
+        logger.warning("Schedule lock acquired by another process. No need to run it in this process.")
     else:
         logger.info("update_n3c_routine()")
         update_n3c_routine()
@@ -42,7 +38,8 @@ def routine():
         daily_schema_update()
         logger.info("daily_coverage_update()")
         daily_coverage_update()
-
+        _lock.release()
+        logger.info("Schedule lock released successfully.")
 
 def run_routine():
     thread = Thread(target=routine, daemon=True)
