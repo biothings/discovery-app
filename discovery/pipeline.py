4641934aba56d31499f1bfa4d0f347a2ec1c5520
Nichollette <nichollette.aco@gmail.com>
2023-08-18 18:15:01 -0400

    removed hardcode filter'

--- discovery/pipeline.py
+++ discovery/pipeline.py
@@ -24,39 +24,30 @@ class DiscoveryQueryBuilder(ESQueryBuilder):
         search = search.update_from_dict(
             {
                 "query": {
-                    "bool": {
-                        "must": {
-                            "function_score": {
-                                "query": {
-                                    "dis_max": {
-                                        "queries": [
-                                            {"term": {"_id": {"value": q, "boost": 15.0}}},
-                                            {"term": {"label.raw": {"value": q, "boost": 10.0}}},
-                                            {"term": {"_meta.username": {"value": q}}},  # for dataset
-                                            {"term": {"name": {"value": q}}},
-                                            {"match": {"parent_classes": {"query": q}}},
-                                            {"prefix": {"label": {"value": q}}},
-                                            {"query_string": {"query": q}},
-                                        ]
-                                    }
-                                },
-                                "functions": [
-                                    {"filter": {"term": {"namespace": "schema"}}, "weight": 0.5},
-                                    {"filter": {"term": {"prefix.raw": "schema"}}, "weight": 0.5},
-                                    {
-                                        "filter": {"match": {"parent_classes": "bts:BiologicalEntity"}},
-                                        "weight": 1.5,
-                                    },
-                                ],
+                    "function_score": {
+                        "query": {
+                            "dis_max": {
+                                "queries": [
+                                    {"term": {"_id": {"value": q, "boost": 15.0}}},
+                                    {"term": {"label.raw": {"value": q, "boost": 10.0}}},
+                                    {"term": {"_meta.username": {"value": q}}},  # for dataset
+                                    {"term": {"name": {"value": q}}},
+                                    {"match": {"parent_classes": {"query": q}}},
+                                    {"prefix": {"label": {"value": q}}},
+                                    {"query_string": {"query": q}},
+                                ]
                             }
                         },
-                        "filter": {
-                            "exists": {
-                                "field": "validation.$schema"
-                            }
-                        }
+                        "functions": [
+                            {"filter": {"term": {"namespace": "schema"}}, "weight": 0.5},
+                            {"filter": {"term": {"prefix.raw": "schema"}}, "weight": 0.5},
+                            {
+                                "filter": {"match": {"parent_classes": "bts:BiologicalEntity"}},
+                                "weight": 1.5,
+                            },
+                        ],
                     }
-                }
+                },
             }
         )
         return search
