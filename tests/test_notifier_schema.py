8c3114dfc20e542a09ac549b20e436e53c7b00c3
Everaldo <everaldorodrigo@gmail.com>
2024-06-18 10:09:34 -0700

    Refactor tests.

--- tests/test_notifier_schema.py
+++ tests/test_notifier_schema.py
@@ -1,18 +1,29 @@
+import os
 import pytest
 
 from unittest.mock import AsyncMock
 from discovery.notify import SchemaNotifier, Message
 
 
+def setup_module(module):
+    os.environ['SLACK_WEBHOOKS'] = 'https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX'
+    os.environ['GA_ACCOUNT'] = 'UA-XXXX-Y'
+    os.environ['GA_UID_GENERATOR_VERSION'] = '0'
+    os.environ['GA4_MEASUREMENT_ID'] = 'G-XXXXXX'
+    os.environ['GA4_API_SECRET'] = 'SECRET'
+    os.environ['GA4_UID_GENERATOR_VERSION'] = '0'
+    os.environ['N3C_AUTH_USER'] = 'n3c_user'
+    os.environ['N3C_AUTH_PASSWORD'] = 'n3c_password'
+
 class MockSettings:
-    SLACK_WEBHOOKS = ["https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX"]
-    GA_ACCOUNT = "UA-XXXX-Y"
-    GA_UID_GENERATOR_VERSION = 1
-    GA4_MEASUREMENT_ID = "G-XXXXXX"
-    GA4_API_SECRET = "SECRET"
-    GA4_UID_GENERATOR_VERSION = 2
-    N3C_AUTH_USER = "n3c_user"
-    N3C_AUTH_PASSWORD = "n3c_password"
+    SLACK_WEBHOOKS = os.getenv("SLACK_WEBHOOKS")
+    GA_ACCOUNT = os.getenv("GA_ACCOUNT")
+    GA_UID_GENERATOR_VERSION = os.getenv("GA_UID_GENERATOR_VERSION")
+    GA4_MEASUREMENT_ID = os.getenv("GA4_MEASUREMENT_ID")
+    GA4_API_SECRET = os.getenv("GA4_API_SECRET")
+    GA4_UID_GENERATOR_VERSION = os.getenv("GA4_UID_GENERATOR_VERSION")
+    N3C_AUTH_USER = os.getenv("N3C_AUTH_USER")
+    N3C_AUTH_PASSWORD = os.getenv("N3C_AUTH_PASSWORD")
 
 
 @pytest.mark.asyncio
