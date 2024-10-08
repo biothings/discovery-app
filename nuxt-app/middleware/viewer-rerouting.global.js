2100d9a6c80ab34aa30dd8011c5090e488b0c5ed
Marco Cano <artofmarco@gmail.com>
2024-10-08 16:30:18 -0700

    fix: :hammer: resource registry filters, pipeline, portals config

--- nuxt-app/middleware/viewer-rerouting.global.js
+++ nuxt-app/middleware/viewer-rerouting.global.js
@@ -2,7 +2,7 @@ export default defineNuxtRouteMiddleware((to, from) => {
   if (to.fullPath.includes("/view") && !to.fullPath.includes("/api")) {
     return navigateTo(to.fullPath.replace("/view", "/ns"));
   }
-  if (to.fullPath.includes("/dataset") && !to.fullPath.includes("/api")) {
-    return navigateTo(to.fullPath.replace("/dataset", "/resource"));
+  if (to.path.includes("/dataset") && !to.fullPath.includes("/api")) {
+    return navigateTo(to.path.replace("/dataset", "/resource"));
   }
 });
