939c955b4fe8ac49a35013ee65f30152392f6e26
Marco Cano <artofmarco@gmail.com>
2024-11-01 10:17:24 -0700

    up node v, fix es issues, add testing libs

--- nuxt-app/pages/guide/Guide.vue
+++ nuxt-app/pages/guide/Guide.vue
@@ -72,10 +72,12 @@
               class="m-0 table table-sm table-light table-hover table-striped text-dark"
             >
               <thead>
-                <th>Name</th>
-                <th>Result/Details</th>
-                <th>Options</th>
-                <th>Status</th>
+                <tr>
+                  <th>Name</th>
+                  <th>Result/Details</th>
+                  <th>Options</th>
+                  <th>Status</th>
+                </tr>
               </thead>
               <colgroup>
                 <col span="1" style="width: 30%" />
---
+++ nuxt-app/vitest.config.mjs
@@ -0,0 +1,11 @@
+// vitest.config.mjs
+import { defineConfig } from 'vitest/config';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+});
