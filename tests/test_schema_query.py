7608707caa57206697550178d79aaf45dcd594da
Nichollette <nichollette.aco@gmail.com>
2025-08-11 14:25:34 -0400

    flake8 and cleaned code for each file, working test examples updated for test examples, backup file added for testing, all tests are passings

--- tests/test_schema_query.py
+++ tests/test_schema_query.py
@@ -15,17 +15,17 @@ class DiscoveryQueryTest(BiothingsTestCase):
         assert res["total"] > 1
 
     def test_query_by_name(self):
-        """GET /schema?q=schema:Library — [QUERY] Customization by name"""
+        """GET /query/?q=schema:Library — [QUERY] Customization by name"""
         self.query(q='\"bts:Google\"')
         self.query(q="Google")  # Case-insensitive label match
         self.query(q="goog")  # Prefix match
 
-    # def test_query_by_property(self):
-    #     """GET /schema?q=name — [QUERY] Match by property field"""
-    #     self.query(q="name")  # property key in 'properties'
-    #     self.query(q="url")  # another property
+    def test_query_by_property(self):
+        """GET /query/?q=species — [QUERY] Match by property field"""
+        self.query(q="funding")
+        self.query(q="species")
 
     def test_query_by_parent(self):
-        """GET /schema?q=schema:Dataset — [QUERY] Match by parent class"""
+        """GET /query/?q=schema:Dataset — [QUERY] Match by parent class"""
         self.query(q='"schema:Dataset"')  # explicit match on parent_classes
         self.query(q="Thing")  # broader term from parent_classes
